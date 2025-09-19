f6c696779408558de84cc9c2adb7e52cb25fd256
annisaa <annisaagustina84@gmail.com>
2025-09-19 11:22:00 +0700

    fixedlist

---
+++ project3/bin/project3.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+ // print('Hello world: ${project3.calculate()}!');
+ var fl = List<int>.filled(4,3);
+ fl[0] = 1;
+ fl[1] = 2;
+ fl[2] = 3;
+ fl[3] = 4;
+ //fl[4] = 5;
+
+ //cetak
+ stdout.writeln(fl);
+
+}
