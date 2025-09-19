a067746e454e67e69d60409b060ade7a8e28ea5e
annisaa <annisaagustina84@gmail.com>
2025-09-19 14:22:28 +0700

    Map

--- project3/bin/project3.dart
+++ project3/bin/project3.dart
@@ -1,7 +1,3 @@
-
-
-import 'dart:io';
-
 void main(List<String> arguments) {
 //  // print('Hello world: ${project3.calculate()}!');
 //  var fl = List<int>.filled(4,3);
@@ -25,20 +21,34 @@ void main(List<String> arguments) {
 // print (setNilai1.union(setNilai2));
 // print (setNilai1.intersection(setNilai2));
 
-var setNilai1 =<String>{};
-  var setNilai2 =<int>{1,2,3};
+// var setNilai1 =<String>{};
+//   var setNilai2 =<int>{1,2,3};
  
- stdout.writeln("Jumlah data setNilai1 = ");
-  String? input1 = stdin.readLineSync();
-  int jumlah1 = (int.tryParse(input1 ?? '')) ?? 0;
-  for (var i = 0; i < jumlah1; i++) {
-    stdout.writeln("Masukkan Nilai Ke-${i+1} = ");
-    String? inputNilai = stdin.readLineSync();
-    String nilai1 = inputNilai ?? '0';
-    setNilai1.add(nilai1);
+//  stdout.writeln("Jumlah data setNilai1 = ");
+//   String? input1 = stdin.readLineSync();
+//   int jumlah1 = (int.tryParse(input1 ?? '')) ?? 0;
+//   for (var i = 0; i < jumlah1; i++) {
+//     stdout.writeln("Masukkan Nilai Ke-${i+1} = ");
+//     String? inputNilai = stdin.readLineSync();
+//     String nilai1 = inputNilai ?? '0';
+//     setNilai1.add(nilai1);
 
-  }
-  var NilaiList = setNilai1.toList();
-  print(NilaiList.elementAt(1));
-  //print(setNilai1.elementAt(0));
+//   }
+//   var NilaiList = setNilai1.toList();
+//   print(NilaiList.elementAt(1));
+    //print(setNilai1.elementAt(0));
+
+    var sudah = {'nim': "101", 'nama': "andi", 'umur': 20};
+    print(sudah);
+    print(sudah['nama']);
+    var opo = {
+      'nim': ["101", "102"],
+      'nama' :["andi", "budi"],
+      'umur' : [20, 21],
+    };
+    print(opo);
+    //print(opo['nama']); mengeprint semua
+    print(opo['nama']![1]); //untuk mengeprint budi saja
+
+
 }
