bcf119a8fa9a0e18faa6b3b244195a5886bccef0
annisaa <annisaagustina84@gmail.com>
2025-10-02 14:46:58 +0700

    tugas praktikum

--- project3/bin/tugas_3.dart
+++ project3/bin/tugas_3.dart
@@ -1,9 +1,9 @@
 
 void main() {
   var barang = <String, Map<String, dynamic>>{
-    'BRG01': {'nama': 'Kipas Angin', 'harga': 90555},
-    'BRG02': {'nama': 'Botol Tupperware', 'harga': 35000},
-    'BRG03': {'nama': 'Bekal Makan', 'harga': 20000},
+    'BRG01': {'nama': 'Tas', 'harga': 150000},
+    'BRG02': {'nama': 'High Heels', 'harga': 75000},
+    'BRG03': {'nama': 'Topi', 'harga': 25000},
   };
 
   print("Data Barang Awal:");
@@ -11,9 +11,9 @@ void main() {
     print('Kode: $kode, Nama: ${data['nama']}, Harga: ${data['harga']}');
   });
 
-  barang['BRG04'] = {'nama': 'Wajan', 'harga': 50000};
-  barang['BRG02'] = {'nama': 'Piring', 'harga': 6000};
-  barang['BRG05'] = {'nama': 'Sendok', 'harga': 2000};
+  barang['BRG04'] = {'nama': 'Skintific', 'harga': 120000};
+  barang['BRG02'] = {'nama': 'Wardah', 'harga': 90000};
+  barang['BRG05'] = {'nama': 'Whitelab', 'harga': 110000};
 
   print("\nData Barang Setelah Update:");
   barang.forEach((kode, data) {
